ba4400299ff006ffe9d076fd95dc44a2ef9d6d98
孟晗 <819239765@qq.com>
2017-02-15 17:55:08 +0800

    Finish Homework 1

--- out/main.js
+++ out/main.js
@@ -1,23 +1,70 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-}());
-window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+window.onload = function () {
+    var c = document.getElementById("mycanvas");
+    var cxt = c.getContext("2d");
+    var stage = new DisplayObjectContainer();
+    var bitmap = new Bitmap();
+    bitmap.Img.src = "aa.png";
+    bitmap.x = 100;
+    bitmap.y = 100;
+    stage.addChild(bitmap);
+    stage.draw(cxt);
+    setInterval(function () {
+        bitmap.x += 10;
+        cxt.clearRect(0, 0, 500, 500);
+        stage.draw(cxt);
+    }, 500);
+};
+var DisplayObjectContainer = (function () {
+    function DisplayObjectContainer() {
+        this.x = 0;
+        this.y = 0;
+        this.CanvasArray = [];
+    }
+    DisplayObjectContainer.prototype.addChild = function (newContext) {
+        this.CanvasArray.push(newContext);
+    };
+    DisplayObjectContainer.prototype.draw = function (context) {
+        for (var _i = 0, _a = this.CanvasArray; _i < _a.length; _i++) {
+            var c = _a[_i];
+            c.draw(context);
+        }
+    };
+    return DisplayObjectContainer;
+}());
+var Bitmap = (function (_super) {
+    __extends(Bitmap, _super);
+    function Bitmap() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.Img = new Image();
+        return _this;
+    }
+    Bitmap.prototype.draw = function (context) {
+        var _this = this;
+        if (!this.Img.onload) {
+            this.Img.onload = function () {
+                context.drawImage(_this.Img, _this.x, _this.y);
+            };
+        }
+        context.drawImage(this.Img, this.x, this.y);
+    };
+    return Bitmap;
+}(DisplayObjectContainer));
+var TextField = (function (_super) {
+    __extends(TextField, _super);
+    function TextField() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.text = "space";
+        return _this;
+    }
+    TextField.prototype.draw = function (context) {
+        var textField = new TextField();
+        context.fillText(this.text, this.x, this.y);
+    };
+    return TextField;
+}(DisplayObjectContainer));
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,66 @@
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+window.onload = () => {
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');    
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
+    var c = document.getElementById("mycanvas") as HTMLCanvasElement
+    var cxt = c.getContext("2d");
+    var stage = new DisplayObjectContainer();
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
+    var bitmap = new Bitmap();
+    bitmap.Img.src = "aa.png"
+    bitmap.x = 100;
+    bitmap.y = 100;
+    stage.addChild(bitmap);
+    stage.draw(cxt);
 
-    stop() {
-        clearTimeout(this.timerToken);
-    }
+    setInterval(() => {
+        bitmap.x += 10;
+        cxt.clearRect(0, 0, 500, 500);
+        stage.draw(cxt);
+    }, 500)
 
+
+};
+
+interface Drawable {
+    draw(context: CanvasRenderingContext2D);
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+class DisplayObjectContainer implements Drawable {
+
+    x: number = 0;
+    y: number = 0;
+
+    CanvasArray: DisplayObjectContainer[] = [];
+
+    addChild(newContext: DisplayObjectContainer) {
+        this.CanvasArray.push(newContext);
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+
+        for (let c of this.CanvasArray) {
+            c.draw(context);
+        }
+    }
+}
+
+class Bitmap extends DisplayObjectContainer {
+    Img = new Image();
+    draw(context: CanvasRenderingContext2D) {
+        if (!this.Img.onload) {
+            this.Img.onload = () => {
+                context.drawImage(this.Img, this.x, this.y);
+            }
+        }
+        context.drawImage(this.Img, this.x, this.y);
+    }
+}
+
+class TextField extends DisplayObjectContainer {
+    text: string = "space";
+
+    draw(context: CanvasRenderingContext2D) {
+        var textField = new TextField();
+
+        context.fillText(this.text, this.x, this.y);
+    }
+}
